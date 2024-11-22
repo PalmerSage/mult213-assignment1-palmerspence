ecac0c1ca0fbd56bbc3ce70d553574341134385a
Palmer Spence <palmerspence@gmail.com>
2024-11-22 12:33:45 -0600

    Initial File Setup
    Added Files to get started

---
+++ index.js
@@ -0,0 +1,13 @@
+// fetch('https://jsonplaceholder.typicode.com/posts')
+//   .then(response => response.json())
+//   .then(data => console.log(data))
+//   .catch(error => console.error('Error:', error))
+
+// fetch('https://restcountries.com/v3.1/name/united')
+//   .then(response => response.json())
+//   .then(data => console.log(data))
+//   .catch(error => console.error('Error:', error))
+
+// fetch('http://dog-api.kinduff.com')
+//   .then(data => console.log(data))
+//   .catch(error => console.error('Error:', error))
